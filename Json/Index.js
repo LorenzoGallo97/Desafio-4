f9c267994d50a6fcb5a19421160cc9dfe018ceef
LorenzoGallo97 <92406178+LorenzoGallo97@users.noreply.github.com>
2022-03-03 23:48:19 -0300

    Add files via upload

---
+++ Json/Index.js
@@ -0,0 +1,13 @@
+<div>
+    <nav>
+        <ul>
+            <li>
+                <a href="./Index.html">Inicio</a>
+                <a href="./Pages/Autovehiculos.html">Autovehiculos</a>
+                <a href="./Pages/Motovehiculos.html">Motovehiculos</a>
+                <a href="./Pages/Registrate.html">Registrate</a>
+                <a href="./Pages/Sobre Nosotros.html">Sobre Nosotros</a>
+            </li>
+        </ul>
+    </nav>  
+</div>
---
+++ Json/formularios.js
@@ -0,0 +1,8 @@
+<footer>
+    <h2>Delivery</h2>
+    <p>Telefono</p>
+    <p>3844-422035</p>
+    <p>Celular</p>
+    <p>3844-436838</p>
+    <p>Copyright: Lorenzo Gallo 2022</p>
+</footer>
